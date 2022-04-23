2cfedd2d30cb5950be1ab4c360871679667d00b6
Dima Polischchuk <dimulka2033@gmail.com>
2022-04-23 13:49:10 +0200

    New change

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,7 @@ const salt = "4t897y2er7b8f1342hg890df21";
 
 process.env.TOKEN_SECRET;
 
-console.log("my newest change");
+console.log("my newest chang number 2");
 
 const getHash = (str: string) => {
   const shasum = crypto.createHash("sha256");
